440aea6030d50e64104f68e20a2ad716f9f23b96
meuhle <alexmuldermuldi@gmail.com>
2023-07-12 14:12:34 +0200

    lateral side

--- mpu6050.ino
+++ mpu6050.ino
@@ -54,9 +54,9 @@ void loop() {
   old_t = current_t;
   // Calculating Roll and Pitch from the accelerometer data
   norm_g =  sqrt(AccX*AccX + AccY*AccY+AccZ*AccZ);
-  acceleration[0] = AccX / norm_g;
+  acceleration[2] = AccX / norm_g;
   acceleration[1]= AccY/norm_g;
-  acceleration[2]=  AccZ/norm_g;
+  acceleration[0]= - AccZ/norm_g;
   //accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - 0.58; // AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
   //accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) + 1.58; // AccErrorY ~(-1.58)
  Serial.print("ERROR X: " + String(GyroErrorX));
@@ -110,22 +110,6 @@ void loop() {
   Serial.print("\nRxEst : ");Serial.print(RwEst[0]);Serial.print("\n");
   Serial.print("RyEst : ");Serial.print(RwEst[1]*-1);Serial.print("\n");
   }
-  // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
-  //gyroAngleX = gyroAngleX + GyroX * elapsedTime; // deg/s * s = deg
-  //gyroAngleY = gyroAngleY + GyroY * elapsedTime;
-  //yaw =  yaw + GyroZ * elapsedTime;
-  // Complementary filter - combine acceleromter and gyro angle values
-  //roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
-  //pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
-  
-  // Print the values on the serial monitor
-  /*Serial.print(roll);
-  Serial.print("/");
-  Serial.print(pitch);
-  Serial.print("/");
-  Serial.println(yaw);*/
-
-
 
 void calculate_IMU_error() {
   // We can call this funtion in the setup section to calculate the accelerometer and gyro data error. From here we will get the error values used in the above equations printed on the Serial Monitor.
